6e9c861360102d0634831eb3ac8fdbe1d675b25b
Yunosuke Sakai <qmkns246@gmail.com>
2026-07-16 15:45:04 +0900

    Initial Commit

---
+++ RenovateSetup/RenovateSetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenovateSetupApp.swift
+//  RenovateSetup
+//
+//  Created by sakai.yunosuke on 2026/07/16.
+//
+
+import SwiftUI
+
+@main
+struct RenovateSetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RenovateSetupTests/RenovateSetupTests.swift
@@ -0,0 +1,18 @@
+//
+//  RenovateSetupTests.swift
+//  RenovateSetupTests
+//
+//  Created by sakai.yunosuke on 2026/07/16.
+//
+
+import Testing
+
+struct RenovateSetupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
